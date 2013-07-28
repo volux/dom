36bf578f895ba88c2acb353a4cebd0bdaa879059
volux <andrey.skulov@gmail.com>
2013-07-28 04:52:34 +0400

    Ooops! Copy - Paster after 200gr Serbian Rackia is Devil

--- lib/volux/Dom/Tag.php
+++ lib/volux/Dom/Tag.php
@@ -6,32 +6,94 @@ use volux\Dom;
     {
 
         /**
-         * Class Set
+         * Class Tag
          * @package volux\Dom
          * @author  Andrey Skulov <andrey.skulov@gmail.com>
          */
-        class Set extends \ArrayIterator implements \RecursiveIterator
+        class Tag extends Element
         {
+            const CLASS_ATTR = 'class';
+
             /**
-             * @var Dom|Html
+             * @param $name
+             * @param null $value
+             *
+             * @return mixed|string|Tag
              */
-            protected $ownerDocument;
-            /**
-             * @param \DOMNodelist|\DOMNamedNodeMap|Set|array $nodeList
-             * @param Dom $dom
-             */
-            public function __construct($nodeList, Dom &$dom)
+            public function data($name, $value = null)
             {
-                $this->ownerDocument = $dom;
-                $nodes = array();
-                foreach ($nodeList as $node) {
-                    $nodes[] = $node;
+                $name = 'data-' . $name;
+                if (is_null($value)) {
+                    return json_decode($this->attr($name)->text());
                 }
-                parent::__construct($nodes);
+                return $this->attr($name, json_encode($value));
             }
 
             /**
-             * @return Dom|Html
+             * attribute for AngularJS directive
+             * @param $name
+             * @param null $value
+             *
+             * @return mixed|string|Tag
+             */
+            public function ng($name, $value = null)
+            {
+                $name = 'ng-' . $name;
+                if (is_null($value)) {
+                    return $this->attr($name)->text();
+                }
+                return $this->attr($name, $value);
+            }
+
+            /**
+             * @param $classes
+             *
+             * @return Tag
+             */
+            public function addClass($classes)
+            {
+                return $this->attrItems(self::CLASS_ATTR, $classes, true);
+            }
+
+            /**
+             * @param bool $classes
+             *
+             * @return Tag
+             */
+            public function removeClass($classes = false)
+            {
+                return $this->attrItems(self::CLASS_ATTR, $classes, false);
+            }
+
+            /**
+             * @param null $html
+             *
+             * @return string|Tag
+             */
+            public function html($html = null)
+            {
+                if (!is_null($html)) {
+                    $this->clear();
+                    return $this->add($html);
+                }
+                return (string)$this->children();
+            }
+
+            /**
+             * @param string $xml
+             *
+             * @return $this|Tag
+             */
+            public function replaceWith($xml)
+            {
+                if ($this->owner()->createFragment($xml, $fragment)) {
+                    return $this->replace($fragment);
+                }
+                return $this;
+            }
+
+            /**
+             * @return Html
              */
             public function owner()
             {
@@ -39,249 +101,12 @@ use volux\Dom;
             }
 
             /**
-             * @return \RecursiveIteratorIterator
-             */
-            public function getRecursiveIterator()
-            {
-                return new \RecursiveIteratorIterator($this, \RecursiveIteratorIterator::SELF_FIRST);
-            }
-
-            /**
-             * @return bool
-             */
-            public function hasChildren()
-            {
-                return $this->current()->hasChildNodes();
-            }
-
-            /**
-             * @return Set
-             */
-            public function getChildren()
-            {
-                return new self($this->current()->childNodes, $this->owner());
-            }
-
-            /**
-             * @param $callable
-             *
-             * @return Set
-             */
-            public function each($callable)
-            {
-                $index = 0;
-                foreach ($this as $node) {
-                    call_user_func($callable, $node, $index++);
-                }
-                return $this;
-            }
-
-            /**
-             * <code> while ($node = $set->sequent()) { $node } </code>
-             * @return Element|Tag|Attr|Text|Cdata
-             */
-            public function sequent()
-            {
-                $this->next();
-                if ($this->valid()) {
-                    return $this->current();
-                }
-                $this->rewind();
-                return false;
-            }
-
-            /**
-             * @param $offset
-             *
-             * @return Element|Tag|Attr|Text|Cdata
-             */
-            public function eq($offset)
-            {
-                if ($this->count() < $offset) {
-                    return $this->owner()->notEmpty(false, '=Set::eq()');
-                }
-                return $this->offsetGet($offset);
-            }
-
-            /**
-             * @return Element|Tag|Attr|Text|Cdata
-             */
-            public function first()
-            {
-                if ($this->count() == 0) {
-                    return $this->owner()->notEmpty(false, '=Set::first()');
-                }
-                return $this->offsetGet(0);
-            }
-
-            /**
-             * @return Element|Tag|Attr
-             */
-            public function last()
-            {
-                if ($this->count() == 0) {
-                    return $this->owner()->notEmpty(false, '=Set::last()');
-                }
-                $offset = $this->count()-1;
-                return $this->offsetGet($offset);
-            }
-
-            /**
-             * @return Set
-             */
-            protected function removeAll()
-            {
-                for ($this->rewind(); $this->valid(); $this->offsetUnset($this->key())) {
-                    $this->offsetGet($this->key())->remove();
-                }
-                return $this;
-            }
-
-            /**
-             * @param null $key
-             *
-             * @return Set
-             */
-            public function remove($key = null)
-            {
-                if (is_null($key)) {
-                    return $this->removeAll();
-                }
-                if (false === $key) {
-                    return $this;
-                }
-                if ($this->offsetExists($key)) {
-                    $this->offsetGet($key)->remove();
-                    $this->offsetUnset($key);
-                    $this->rewind();
-                }
-                return $this;
-            }
-
-            /**
-             * @param string|Element|Tag $target
-             *
-             * @return $this
-             */
-            public function appendTo(&$target)
-            {
-                if (is_string($target)) {
-                    $target = $this->owner()->createElement($target);
-                }
-                foreach ($this as $node) {
-                    $target->append($node);
-                }
-                return $this;
-            }
-
-            /**
-             * @return bool
-             */
-            public function isEmpty()
-            {
-                return ($this->count() === 0);
-            }
-
-            /**
-             * @param string $xPath to child nodes like "date" or "@date"
-             *
-             * @return Set
-             */
-            public function sortBy($xPath)
-            {
-                $comparator = function (Element $a, Element $b) use ($xPath) {
-                    $aValue = $a->find($xPath, 0)->text();
-                    $bValue = $b->find($xPath, 0)->text();
-                    if (!is_numeric($aValue)) {
-                        return strcasecmp($aValue, $bValue);
-                    }
-                    if ($aValue == $bValue) {
-                        return 0;
-                    }
-                    return ($aValue < $bValue) ? -1 : 1;
-                };
-                $this->uasort($comparator);
-                return $this;
-            }
-
-            /**
-             * @param $selector
-             *
-             * @return Set
-             */
-            public function filter($selector)
-            {
-                $nodeset = array();
-                foreach ($this as $node) {
-                    /** @var $node Element|Tag|Attr|Text|Cdata */
-                    if ($node->is($selector)) {
-                        $nodeset[] = $node;
-                    }
-                }
-                return new self($nodeset, $this->owner());
-            }
-
-            /**
-             * also used like $this->names('attr', 'id')
-             *
-             * @param string $method
-             * @param string $key
-             * @param null $arg
-             *
-             * @return \ArrayIterator
-             */
-            public function map($method = 'text', $key = 'getNodePath', $arg = null)
-            {
-                $array = array();
-                foreach ($this as $node) {
-                    /** @var $node Element|Tag|Attr|Text|Cdata */
-                    $array[$node->$key()] = $node->$method($arg);
-                }
-                return new \ArrayIterator($array);
-            }
-
-            /**
-             * @param string $xslFile
-             * @param array $xsltParameters
-             *
-             * @return $this
-             */
-            public function transform($xslFile, $xsltParameters = array())
-            {
-                foreach ($this as $node) {
-                    $this->owner()->transform($xslFile, $xsltParameters, $node);
-                }
-                return $this;
-            }
-
-            /**
-             * Invoke $method for each node in Set
-             *
-             * @param $method
-             * @param array $args
-             *
-             * @return Set
-             */
-            public function __call($method, array $args)
-            {
-                foreach ($this as $node) {
-                    /** @var $node Element|Tag|Attr|Text|Cdata */
-                    call_user_func_array(array($node, $method), $args);
-                }
-                return $this;
-            }
-
-            /**
              * @return string
              */
             public function __toString()
             {
-                $string = '';
-                foreach ($this as $node) {
-                    /** @var $node Element|Tag|Attr|Text|Cdata */
-                    $string .= (string)$node. PHP_EOL . PHP_EOL;
-                }
-                return $string;
+                return $this->ownerDocument->saveHTML($this);
             }
         }
+
     }
