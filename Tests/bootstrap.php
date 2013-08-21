fe4a52725fbde480ccf8229237a3af8d5534649e
volux <andrey.skulov@gmail.com>
2013-08-21 21:05:15 +0400

    Fix bootstrap.php loader()

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,11 +2,12 @@
 function loader($className)
 {
     $paths = array(
-        'Tests' => '.',
-        'volux' => '../src',
+        'Tests' => 'Tests/',
+        'volux' => 'src/volux/',
     );
     $path = explode('\\', str_replace('_', '\\', $className));
     $file = $paths[array_shift($path)].implode(DIRECTORY_SEPARATOR, $path).'.php';
+    var_dump($file);
     require $file;
 }
 
